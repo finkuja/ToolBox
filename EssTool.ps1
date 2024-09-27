c6b9d28ad653ca471b481b7f97123066e36665ec
Carlos Alvarez Magariños <carlos.alvarez@microsoft.com>
2024-09-27 15:43:30 +0200

    Update EssTool.ps1

--- EssTool.ps1
+++ EssTool.ps1
@@ -1,6 +1,13 @@
 # Ensure you run this script with administrator privileges
 # Import necessary .NET assemblies
 
+Write-Output "  ______  _____  _____    _______          _ 
+ |  ____|/ ____|/ ____|  |__   __|        | |
+ | |__  | (___ | (___       | | ___   ___ | |
+ |  __|  \___ \ \___ \      | |/ _ \ / _ \| |
+ | |____ ____) |____) |     | | (_) | (_) | |
+ |______|_____/|_____/      |_|\___/ \___/|_|"
+
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
