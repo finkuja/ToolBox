41762b5d3e91345cadd934aa360cf172e2484a42
Carlos Alvarez <caralv@microsoft.com>
2024-09-27 17:26:11 +0200

    Adding Tabs to the EssTool.ps1 script

--- EssTool.ps1
+++ EssTool.ps1
@@ -16,32 +16,43 @@ $form.Text = "ESS Tool Box a.01"
 $form.Size = New-Object System.Drawing.Size(400, 300)
 $form.StartPosition = "CenterScreen"
 
-# Create checkboxes for packages
+# Create a TabControl
+$tabControl = New-Object System.Windows.Forms.TabControl
+$tabControl.Size = New-Object System.Drawing.Size(380, 250)
+$tabControl.Location = New-Object System.Drawing.Point(10, 10)
+$form.Controls.Add($tabControl)
+
+# Create the Install tab
+$tabInstall = New-Object System.Windows.Forms.TabPage
+$tabInstall.Text = "Install / Update"
+$tabControl.Controls.Add($tabInstall)
+
+# Create checkboxes for packages in the Install tab
 $checkboxOffice = New-Object System.Windows.Forms.CheckBox
 $checkboxOffice.Text = "Microsoft Office 365"
 $checkboxOffice.Location = New-Object System.Drawing.Point(20, 20)
-$form.Controls.Add($checkboxOffice)
+$tabInstall.Controls.Add($checkboxOffice)
 
 $checkboxPowerToys = New-Object System.Windows.Forms.CheckBox
 $checkboxPowerToys.Text = "PowerToys"
 $checkboxPowerToys.Location = New-Object System.Drawing.Point(20, 50)
-$form.Controls.Add($checkboxPowerToys)
+$tabInstall.Controls.Add($checkboxPowerToys)
 
 $checkboxTeams = New-Object System.Windows.Forms.CheckBox
 $checkboxTeams.Text = "Microsoft Teams"
 $checkboxTeams.Location = New-Object System.Drawing.Point(20, 80)
-$form.Controls.Add($checkboxTeams)
+$tabInstall.Controls.Add($checkboxTeams)
 
 $checkboxOneNote = New-Object System.Windows.Forms.CheckBox
 $checkboxOneNote.Text = "Microsoft OneNote"
 $checkboxOneNote.Location = New-Object System.Drawing.Point(20, 110)
-$form.Controls.Add($checkboxOneNote)
+$tabInstall.Controls.Add($checkboxOneNote)
 
-# Create an Install button
+# Create an Install button in the Install tab
 $buttonInstall = New-Object System.Windows.Forms.Button
 $buttonInstall.Text = "Install"
 $buttonInstall.Location = New-Object System.Drawing.Point(20, 160)
-$form.Controls.Add($buttonInstall)
+$tabInstall.Controls.Add($buttonInstall)
 
 # Define the action for the Install button
 $buttonInstall.Add_Click({
@@ -59,6 +70,22 @@ $buttonInstall.Add_Click({
     }
     [System.Windows.Forms.MessageBox]::Show("Selected packages have been installed.")
 })
+
+# Create the Tweak tab
+$tabTweak = New-Object System.Windows.Forms.TabPage
+$tabTweak.Text = "Tweak"
+$tabControl.Controls.Add($tabTweak)
+
+# Create controls for the Tweak tab
+# ...
+
+# Create the Fix tab
+$tabFix = New-Object System.Windows.Forms.TabPage
+$tabFix.Text = "Fix"
+$tabControl.Controls.Add($tabFix)
+
+# Create controls for the Fix tab
+# ...
 
 # Run the form
 $form.ShowDialog()
