b8b48d1a52fd688fd035279964854bfcf5cca007
finkuja <91827854+finkuja@users.noreply.github.com>
2024-10-10 12:45:31 +0200

    Update EssTool.ps1

--- EssTool.ps1
+++ EssTool.ps1
@@ -1653,14 +1653,29 @@ $buttonApply.Add_Click({
             Write-Host "Optimizing drives..." -ForegroundColor Green
             Get-Volume | Where-Object { $_.DriveLetter } | ForEach-Object { 
                 try {
-                    $mediaType = (Get-PhysicalDisk | Where-Object { $_.DeviceID -eq $_.DriveLetter }).MediaType
-                    if ($mediaType -eq 'SSD') {
+                    $partition = Get-Partition -DriveLetter $_.DriveLetter -ErrorAction SilentlyContinue
+                    if ($null -eq $partition) {
+                        Write-Host "No partition found for drive $($_.DriveLetter). Skipping optimization." -ForegroundColor Yellow
+                        return
+                    }
+        
+                    $physicalDisk = Get-PhysicalDisk | Where-Object { $_.DeviceID -eq $partition.DiskNumber }
+                    if ($null -eq $physicalDisk) {
+                        Write-Host "No physical disk found for partition $($partition.DiskNumber). Skipping optimization." -ForegroundColor Yellow
+                        return
+                    }
+        
+                    $mediaType = $physicalDisk.MediaType
+                    if ($mediaType -eq 'SSD' -or $mediaType -eq 'Solid State Drive') {
                         Optimize-Volume -DriveLetter $_.DriveLetter -ReTrim -Verbose
                         Write-Host "SSD Drive $($_.DriveLetter) has been optimized with ReTrim." -ForegroundColor Green
                     }
-                    else {
+                    elseif ($mediaType -eq 'HDD' -or $mediaType -eq 'Hard Disk Drive') {
                         Optimize-Volume -DriveLetter $_.DriveLetter -Defrag -Verbose
-                        Write-Host "Drive $($_.DriveLetter) has been optimized with Defrag." -ForegroundColor Green
+                        Write-Host "HDD Drive $($_.DriveLetter) has been optimized with Defrag." -ForegroundColor Green
+                    }
+                    else {
+                        Write-Host "Unknown media type for drive $($_.DriveLetter). Skipping optimization." -ForegroundColor Yellow
                     }
                 }
                 catch {
