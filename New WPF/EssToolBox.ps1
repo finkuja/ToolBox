08fa0c7513b42d049ffaa2022d6da771f881d6fb
finkuja <25007711+caralv@users.noreply.github.com>
2024-10-13 02:20:14 +0200

    dbg

--- New WPF/EssToolBox.ps1
+++ New WPF/EssToolBox.ps1
@@ -250,7 +250,6 @@ Read-Host "Stop 2"
 # Check if the XAML folder exists and the Functions folder exists
 if (-not (Test-Path -Path $xamlDir -PathType Container) -or -not (Test-Path -Path $functionsDir -PathType Container)) {
     Write-Host "The XAML or Functions folder cannot be found." -ForegroundColor Red
-    Read-Host "Stop 3"
     if ($tempDir) {
         Write-Host "Running from a temporary directory. Attempting to download XAML and Functions from GitHub..." -ForegroundColor Yellow
         
@@ -262,11 +261,12 @@ if (-not (Test-Path -Path $xamlDir -PathType Container) -or -not (Test-Path -Pat
         # Define the paths to the XAML and Functions folders in the GitHub repository
         $xamlPath = "New WPF/XAML/MainWindow.xml"
         $functionsPath = "New WPF/Functions"
-        
+        Read-Host "Stop 0"
         # Define the local paths to the XAML and Functions folders
         $xamlDir = [System.IO.Path]::Combine($scriptDir, "XAML")
+        Read-Host "Stop 1"
         $functionsDir = [System.IO.Path]::Combine($scriptDir, "Functions")
-        Read-Host "Stop 4"
+        
         # Create the local XAML and Functions directories if they do not exist
         if (-not (Test-Path -Path $xamlDir)) {
             New-Item -ItemType Directory -Path $xamlDir | Out-Null
