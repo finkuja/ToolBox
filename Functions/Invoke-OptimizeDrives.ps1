45b0c60d55b618f38afefad26bdb51f60f287b4e
finkuja <25007711+caralv@users.noreply.github.com>
2024-10-15 23:37:42 +0200

    Fix Bug with optimize drives and remove unused function

--- Functions/Invoke-OptimizeDrives.ps1
+++ Functions/Invoke-OptimizeDrives.ps1
@@ -34,50 +34,49 @@ based on their media type.
 #>
 function Invoke-OptimizeDrives {
     # Optimize Drives
-    function Invoke-DeleteTempFiles {
-        $confirmation = [System.Windows.Forms.MessageBox]::Show(
-            "Are you sure you want to optimize the drives? This operation can take a long time depending on the type of drive.",
-            "Confirmation",
-            [System.Windows.Forms.MessageBoxButtons]::YesNo,
-            [System.Windows.Forms.MessageBoxIcon]::Warning
-        )
+    $confirmation = [System.Windows.Forms.MessageBox]::Show(
+        "Are you sure you want to optimize the drives? This operation can take a long time depending on the type of drive.",
+        "Confirmation",
+        [System.Windows.Forms.MessageBoxButtons]::YesNo,
+        [System.Windows.Forms.MessageBoxIcon]::Warning
+    )
     
-        if ($confirmation -ne [System.Windows.Forms.DialogResult]::Yes) {
-            Write-Host "Operation cancelled by the user."
-            return
-        }
-        Write-Host "Optimizing drives..." -ForegroundColor Green
-        Get-Volume | Where-Object { $_.DriveLetter } | ForEach-Object { 
-            try {
-                $partition = Get-Partition -DriveLetter $_.DriveLetter -ErrorAction SilentlyContinue
-                if ($null -eq $partition) {
-                    Write-Host "No partition found for drive $($_.DriveLetter). Skipping optimization." -ForegroundColor Yellow
-                    return
-                }
-
-                $physicalDisk = Get-PhysicalDisk | Where-Object { $_.DeviceID -eq $partition.DiskNumber }
-                if ($null -eq $physicalDisk) {
-                    Write-Host "No physical disk found for partition $($partition.DiskNumber). Skipping optimization." -ForegroundColor Yellow
-                    return
-                }
-
-                $mediaType = $physicalDisk.MediaType
-                if ($mediaType -eq 'SSD' -or $mediaType -eq 'Solid State Drive') {
-                    Optimize-Volume -DriveLetter $_.DriveLetter -ReTrim -Verbose
-                    Write-Host "SSD Drive $($_.DriveLetter) has been optimized with ReTrim." -ForegroundColor Green
-                }
-                elseif ($mediaType -eq 'HDD' -or $mediaType -eq 'Hard Disk Drive') {
-                    Optimize-Volume -DriveLetter $_.DriveLetter -Defrag -Verbose
-                    Write-Host "HDD Drive $($_.DriveLetter) has been optimized with Defrag." -ForegroundColor Green
-                }
-                else {
-                    Write-Host "Unknown media type for drive $($_.DriveLetter). Skipping optimization." -ForegroundColor Yellow
-                }
-            }
-            catch {
-                Write-Host "Failed to optimize drive $($_.DriveLetter). Error: $_" -ForegroundColor Yellow
-            }
-        }
-        Write-Host "Drive optimization process completed." -ForegroundColor Green
+    if ($confirmation -ne [System.Windows.Forms.DialogResult]::Yes) {
+        Write-Host "Operation cancelled by the user."
+        return
     }
-} 
+    Write-Host "Optimizing drives..." -ForegroundColor Green
+    Get-Volume | Where-Object { $_.DriveLetter } | ForEach-Object { 
+        try {
+            $partition = Get-Partition -DriveLetter $_.DriveLetter -ErrorAction SilentlyContinue
+            if ($null -eq $partition) {
+                Write-Host "No partition found for drive $($_.DriveLetter). Skipping optimization." -ForegroundColor Yellow
+                return
+            }
+
+            $physicalDisk = Get-PhysicalDisk | Where-Object { $_.DeviceID -eq $partition.DiskNumber }
+            if ($null -eq $physicalDisk) {
+                Write-Host "No physical disk found for partition $($partition.DiskNumber). Skipping optimization." -ForegroundColor Yellow
+                return
+            }
+
+            $mediaType = $physicalDisk.MediaType
+            if ($mediaType -eq 'SSD' -or $mediaType -eq 'Solid State Drive') {
+                Optimize-Volume -DriveLetter $_.DriveLetter -ReTrim -Verbose
+                Write-Host "SSD Drive $($_.DriveLetter) has been optimized with ReTrim." -ForegroundColor Green
+            }
+            elseif ($mediaType -eq 'HDD' -or $mediaType -eq 'Hard Disk Drive') {
+                Optimize-Volume -DriveLetter $_.DriveLetter -Defrag -Verbose
+                Write-Host "HDD Drive $($_.DriveLetter) has been optimized with Defrag." -ForegroundColor Green
+            }
+            else {
+                Write-Host "Unknown media type for drive $($_.DriveLetter). Skipping optimization." -ForegroundColor Yellow
+            }
+        }
+        catch {
+            Write-Host "Failed to optimize drive $($_.DriveLetter). Error: $_" -ForegroundColor Yellow
+        }
+    }
+    Write-Host "Drive optimization process completed." -ForegroundColor Green
+}
+ 
