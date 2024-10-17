6afa944fe1dd541075bb9d044b6d2488ea5bac7a
finkuja <25007711+caralv@users.noreply.github.com>
2024-10-17 15:25:47 +0200

    Bug Fixes

--- Functions/Invoke-SystemTroubleshoot.ps1
+++ Functions/Invoke-SystemTroubleshoot.ps1
@@ -19,5 +19,5 @@
 function Invoke-SystemTroubleshoot {
     # Open Windows Other Troubleshooters settings page
     Write-Host "Opening Windows Other Troubleshooters..." -ForegroundColor Yellow
-    Start-Process ms-settings:troubleshoot-other
+    Start-Process ms-settings:troubleshoot
 }
